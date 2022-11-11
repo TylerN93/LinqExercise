3c87dcc50f03745e8ccf35a952ee1f3fbfc4f42a
TylerN93 <tylernewton73@gmail.com>
2022-11-10 22:55:24 -0600

    Completed

--- LinqExercise/Program.cs
+++ LinqExercise/Program.cs
@@ -22,33 +22,119 @@ namespace LinqExercise
 
             //TODO: Print the Sum of numbers
 
+            Console.WriteLine($"The sum is {numbers.Sum()}");
+
             //TODO: Print the Average of numbers
+
+            Console.WriteLine($" The average is {numbers.Average()}");
 
             //TODO: Order numbers in ascending order and print to the console
 
+            var asc = numbers.OrderBy(num => num);
+
+            Console.WriteLine($"Ascending:");
+
+            foreach (var item in asc)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            Console.WriteLine("-----------");
+
             //TODO: Order numbers in decsending order and print to the console
+            var desc = numbers.OrderByDescending(num => num);
+
+            Console.WriteLine($"Descending:");
+
+
+            foreach (var item in desc)
+            {
+                Console.WriteLine($"{item}");
+            }
 
             //TODO: Print to the console only the numbers greater than 6
 
+            Console.WriteLine($"Greater than 6:");
+
+            var greater = numbers.Where(num => num > 6);
+
+            foreach(var item in greater)
+            {
+                Console.WriteLine($"{item}");
+            }
+
             //TODO: Order numbers in any order (acsending or desc) but only print 4 of them **foreach loop only!**
 
+            Console.WriteLine("Take 4");
+
+            foreach (var num in asc.Take(4))
+            {
+                Console.WriteLine($"{num}");
+            }
+
             //TODO: Change the value at index 4 to your age, then print the numbers in decsending order
+
+            numbers[4] = 29;
+
+            Console.WriteLine($"-------");
+
+            foreach (var item in numbers.OrderByDescending(num=> num))
+            {
+                Console.WriteLine($"{item}");
+            }
 
             // List of employees ****Do not remove this****
             var employees = CreateEmployees();
 
             //TODO: Print all the employees' FullName properties to the console only if their FirstName starts with a C OR an S and order this in ascending order by FirstName.
+            
+            var filtered = employees.Where(x => x.FirstName.StartsWith('C') || x.FirstName.StartsWith('S')).OrderBy(x => x.FirstName);
+
+            Console.WriteLine(" C or S employees");
+
+            foreach (var item in filtered)
+            {
+                Console.WriteLine($"{item.FullName}");
+            }
 
             //TODO: Print all the employees' FullName and Age who are over the age 26 to the console and order this by Age first and then by FirstName in the same result.
 
+            Console.WriteLine("--------");
+            Console.WriteLine("Over 26");
+
+            var overTwentySix = employees.Where(x => x.Age > 26).OrderBy(x => x.Age).ThenBy(x => x.FirstName);
+
+            foreach(var item in overTwentySix)
+            {
+                Console.WriteLine($"{item.FullName} {item.Age}");
+            }
+
             //TODO: Print the Sum and then the Average of the employees' YearsOfExperience if their YOE is less than or equal to 10 AND Age is greater than 35.
 
+            Console.WriteLine("--------");
+
+            var years = employees.Where(x => x.YearsOfExperience <= 10 && x.Age > 35);
+
+            var sum = years.Sum(x => x.YearsOfExperience);
+
+            var avg = years.Average(x => x.YearsOfExperience);
+
+            Console.WriteLine($" Sum: {sum} Average: {avg}");
+
             //TODO: Add an employee to the end of the list without using employees.Add()
+
+            employees = employees.Append(new Employee("Tyler", "Newton", 29, 1)).ToList();
+
+            foreach (var item in employees)
+            {
+                Console.WriteLine($" {item.FirstName}, {item.Age}");
+            }
 
 
             Console.WriteLine();
 
             Console.ReadLine();
+            {}
         }
 
         #region CreateEmployeesMethod
